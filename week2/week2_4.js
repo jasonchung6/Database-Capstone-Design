443f4dd631f6a30743713b965a9ee167a83b7764
jasonchung6 <ymca6987@gmail.com>
2023-09-04 16:25:29 +0900

    1625

--- week2/week2_4.js
+++ week2/week2_4.js
@@ -1,18 +1,19 @@
-const num = 1
-const str = '1'
-
-if (num === str){
-    console.log('true');
-}else{
-    console.log('false');
-}
-
-if (num == str) {
-    console.log('true');
-} else {
-    console.log('false');
-}
-
-const hello = (name) =>console.log(`Hello, ${name}!`);
-
-hello('Jason!');
+const firstName = document.getElementById("firstname");
+const lastName = document.getElementById("lastname");
+const email = document.getElementById("mail");
+const userid = document.getElementById("userid");
+const printForm = document.getElementById("user");
+const display = document.getElementById("form-result");
+const handlePrint = (e) => {
+    e.preventDefault(); // Prevents the window from moving
+    const fn = firstName.value;
+    const ln = lastName.value;
+    const em = email.value;
+    const id = userid.value;
+    const diplaySpan = display.querySelector("span"); // Select the span tag inside the tag with id form-result
+    diplaySpan.innerHTML = `First Name is: ${fn}<br>
+    Last Name is: ${ln}<br>
+    E-mail is: ${em}<br>
+    ID is: ${id}`;
+};
+printForm.addEventListener("submit", handlePrint);
