b418a192064570c046bfe52147639f3505235eda
jasonchung6 <ymca6987@gmail.com>
2023-09-04 15:53:58 +0900

    09041553

---
+++ week2/week2_4.js
@@ -0,0 +1,18 @@
+const num = 1
+const str = '1'
+
+if (num === str){
+    console.log('true');
+}else{
+    console.log('false');
+}
+
+if (num == str) {
+    console.log('true');
+} else {
+    console.log('false');
+}
+
+const hello = (name) =>console.log(`Hello, ${name}!`);
+
+hello('Jason!');
